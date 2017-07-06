d58e6e05c5ad3c3b4a42623e509695f9673f7c32
Odia Oghenevwede <odiaoghenevwede@gmail.com>
2017-07-06 21:17:51 +0100

    added mongo URI

--- server/mongoUtil.js
+++ server/mongoUtil.js
@@ -6,7 +6,7 @@ let _db;
 
 module.exports = {
 connect() {
-client.connect('mongodb://ds147902.mlab.com:47902/heroku_3mt9tn4j', (err, db) => {
+client.connect('mongodb://odia:Odia1234@ds147902.mlab.com:47902/heroku_3mt9tn4j', (err, db) => {
 if(err) {
 console.log("Error connecting to Mongo");
 process.exit(1);
